8c7df87e4a8d954c2066376f6ed73b7f510be273
Jaydeep Sarkar <jaydeepsarkr@gmail.com>
2025-06-29 20:34:10 +0530

    fixed cors

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
